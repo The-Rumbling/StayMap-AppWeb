b5741f491171b8ef527b17856a049be3c1341747
Antuanete01 <u202310358@upc.edu.pe>
2025-05-16 20:09:46 -0500

    Update community.service.ts

--- src/app/community/services/community.service.ts
+++ src/app/community/services/community.service.ts
@@ -1,18 +1,36 @@
 import { Injectable } from '@angular/core';
-import {environment} from '../../../environments/environment';
-import {BaseService} from '../../shared/services/base.service';
-import {Community} from '../model/community.entity';
-import {catchError, map, Observable} from 'rxjs';
+import { environment } from '../../../environments/environment';
+import { BaseService } from '../../shared/services/base.service';
+import { Community } from '../model/community.entity';
+import { catchError, map, Observable } from 'rxjs';
 
 const communityResourceEndPoint = environment.communityEndpointPath || '';
 
 @Injectable({
   providedIn: 'root'
 })
-export class CommunityService extends BaseService<Community>{
-
+export class CommunityService extends BaseService<Community> {
   constructor() {
     super();
     this.resourceEndpoint = communityResourceEndPoint;
+  }
+
+  override getAll(): Observable<Community[]> {
+    return this.http.get<any>(this.resourcePath(), this.httpOptions).pipe(
+      map(data => {
+        // Asegúrate de que tu JSON tiene una clave "communities"
+        const communitiesArray = Array.isArray(data)
+          ? data
+          : (data.communities || []);
+
+        return communitiesArray.map((item: any) => new Community({
+          id: item.id,
+          name: item.name || item.nombre || '',
+          memberQuantity: item.memberQuantity || item.cantMiembros || '',
+          image: item.image || item.imagen || ''
+        }));
+      }),
+      catchError(this.handleError)
+    );
   }
 }
