07976a599d64bd90e463df47a1a33c94ce5da6a4
Antuanete01 <u202310358@upc.edu.pe>
2025-05-16 19:49:04 -0500

    Update community.service.ts

--- src/app/community/services/community.service.ts
+++ src/app/community/services/community.service.ts
@@ -15,4 +15,29 @@ export class CommunityService extends BaseService<Community>{
     super();
     this.resourceEndpoint = communityResourceEndPoint;
   }
+
+override getAll(): Observable<Community[]> {
+  return this.http.get<any>(this.resourcePath(), this.httpOptions).pipe(
+    map(data => {
+      let communitiesArray = [];
+
+      if (Array.isArray(data)) {
+        communitiesArray = data;
+      } else if ('communities' in data && Array.isArray(data.communities)) {
+        communitiesArray = data.communities;
+      } else {
+        console.error("❌ El JSON no tiene el formato esperado.");
+      }
+
+      return communitiesArray.map((item: any) => new Community({
+        id: item.id,
+        name: item.name || item.nombre || '',
+        memberQuantity: item.memberQuantity || item.cantMiembros || '',
+        image: item.image || item.imagen || ''
+      }));
+    }),
+    catchError(this.handleError)
+  );
+}
+
 }
