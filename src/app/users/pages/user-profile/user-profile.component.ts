810cf2ae21355e6338524ff25e7093adfcde66c5
Antuanete01 <u202310358@upc.edu.pe>
2025-05-17 12:24:56 -0500

    Add files via upload

---
+++ src/app/users/pages/user-profile/user-profile.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import {UserService} from '../../services/user.service';
+import {User} from '../../model/user.entity';
+import {Router} from '@angular/router';
+
+@Component({
+  selector: 'app-user-profile',
+  imports: [],
+  templateUrl: './user-profile.component.html',
+  styleUrl: './user-profile.component.css'
+})
+export class UserProfileComponent {
+  currentUser: User = new User({});
+  constructor(private userService: UserService, private router: Router) {
+    this.userService.currentUser$.subscribe(user => {
+      this.currentUser = user;
+    });
+  }
+
+  onLogout() {
+    this.userService.logout();
+    this.router.navigate(['/concerts']);
+  }
+}
