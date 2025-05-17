66c2c696749eab399021ec1ff6c0f98148c90523
Antuanete01 <u202310358@upc.edu.pe>
2025-05-17 12:11:30 -0500

    Update user.service.ts

--- src/app/users/services/user.service.ts
+++ src/app/users/services/user.service.ts
@@ -2,27 +2,26 @@ import { Injectable } from '@angular/core';
 import { BaseService } from '../../shared/services/base.service';
 import { User } from '../model/user.entity';
 import { BehaviorSubject, catchError, map, of, retry } from 'rxjs';
-import { HttpClient } from '@angular/common/http';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService extends BaseService<User> {
 
-  private currentUserSubject = new BehaviorSubject<any>(this.getCurrentUser());
+  private currentUserSubject = new BehaviorSubject<User | null>(this.getCurrentUser());
   public currentUser$ = this.currentUserSubject.asObservable();
 
   constructor() {
     super();
-    // Reemplaza con tu ruta real si cambia
-    this.resourceEndpoint = '/assets/data/db.json';
+    this.resourceEndpoint = '/assets/data/db.json'; // Lee el archivo completo
   }
 
   override login(email: string, password: string) {
     return this.http.get<any>(this.resourcePath(), this.httpOptions).pipe(
       map(data => {
-        const users = data.users || [];
-        const user = users.find((u: any) => u.email === email && u.password === password);
+        const users = data.users || []; // 👈 accede a users del objeto
+        const user = users.find((u: User) => u.email === email && u.password === password);
         if (user) {
           localStorage.setItem('currentUser', JSON.stringify(user));
           this.currentUserSubject.next(user);
@@ -39,7 +38,7 @@ export class UserService extends BaseService<User> {
     this.currentUserSubject.next(null);
   }
 
-  getCurrentUser() {
+  getCurrentUser(): User | null {
     return JSON.parse(localStorage.getItem('currentUser') || 'null');
   }
 
