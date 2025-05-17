73100ea79ba49b36c5c4de92fd4209ea3d39c362
Antuanete01 <u202310358@upc.edu.pe>
2025-05-17 12:47:34 -0500

    Update app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,16 +1,16 @@
 import { Routes } from '@angular/router';
 import {CommunityManagementComponent} from './community/pages/community-management/community-management.component';
-import {ConcertViewComponent} from './concerts/pages/concert-view/concert-view.component';
 import {ConcertMapComponent} from './concerts/pages/concert-map/concert-map.component';
 import {LoginComponent} from './public/pages/login/login.component';
 import {RegisterComponent} from './public/pages/register/register.component';
 import {UserProfileComponent} from './users/pages/user-profile/user-profile.component';
+import {ConcertManagementComponent} from './concerts/pages/concert-management/concert-management.component';
 
 export const routes: Routes = [
   { path: 'communities',  component: CommunityManagementComponent },
   { path: 'map', component:  ConcertMapComponent },
-   { path: 'concerts', component:  ConcertViewComponent },
+  { path: 'concerts', component:  ConcertManagementComponent },
   { path: 'login', component:  LoginComponent },
-  { path: 'register', component: RegisterComponent }
-
+  { path: 'register', component: RegisterComponent },
+  { path: 'profile', component: UserProfileComponent },
 ];
