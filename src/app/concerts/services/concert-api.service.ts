92590da711e82162719e44c64171201364b00ea1
Antuanete01 <u202310358@upc.edu.pe>
2025-05-16 19:59:30 -0500

    Update concert-api.service.ts

--- src/app/concerts/services/concert-api.service.ts
+++ src/app/concerts/services/concert-api.service.ts
@@ -1,24 +1,18 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { ConcertResponse } from './concert.response';
-import { map } from 'rxjs';
-import { ConcertAssembler } from './concert.assembler';
+import {HttpClient} from '@angular/common/http';
+import {ConcertResponse} from './concert.response';
+import {map} from 'rxjs';
+import {ConcertAssembler} from './concert.assembler';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ConcertApiService {
-  // ⚠️ IMPORTANTE: en producción esto será simplemente '/db.json'
-  private dbUrl: string = '/db.json';
-
-  constructor(private http: HttpClient) {}
-
+  private concertUrl: string = 'http://localhost:3000/concerts';
+  constructor(private http: HttpClient) { }
   getConcerts() {
-    return this.http.get<any>(this.dbUrl).pipe(
-      map(response => {
-        const concertResponse: ConcertResponse = response.concerts; // extrae "concerts"
-        return ConcertAssembler.toEntitiesFromResponse(concertResponse);
-      })
+    return this.http.get<ConcertResponse>(`${this.concertUrl}`).pipe(
+      map(response=>ConcertAssembler.toEntitiesFromResponse(response))
     );
   }
 }
