ba4b39d4d1f7923e43e5e56937916ca273e36ab3
Antuanete01 <u202310358@upc.edu.pe>
2025-05-17 13:14:47 -0500

    Update environment.development.ts

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -3,6 +3,7 @@ export const environment = {
   // Server Base URL for Rest API
   serverBaseUrl: '',
   communityEndpointPath: 'assets/data/db.json',
+  concertsEndpointPath: 'assets/data/db.json',
   usersEndpointPath: 'assets/data/db.json'
 };
 
