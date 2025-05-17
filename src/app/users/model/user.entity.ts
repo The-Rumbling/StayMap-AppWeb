e5e277929309995fa40cdb4414939fd42a69b653
D4D3v4l <diegocollantes202010@gmail.com>
2025-05-17 13:13:50 -0500

    Update user.entity.ts

--- src/app/users/model/user.entity.ts
+++ src/app/users/model/user.entity.ts
@@ -2,6 +2,7 @@ export class User {
   id: number;
   name: string;
   email: string;
+  password: string;
   profileImage: string;
   type: string;
   location:{
@@ -9,12 +10,13 @@ export class User {
     lng:number;
   }
 
-  constructor(user:{id?:number, name?:string, email?:string, profileImage?:string, type?:string, location?:{lat:number, lng:number}}) {
+  constructor(user:{id?:number, name?:string, email?:string, profileImage?:string, type?:string, location?:{lat:number, lng:number}, password?:string}) {
     this.id = user.id || 0;
     this.name = user.name || "";
     this.email = user.email || "";
     this.profileImage = user.profileImage || "";
     this.type = user.type || "";
     this.location = user.location || {lat:0, lng:0 };
+    this.password=user.password || "";
   }
 }
