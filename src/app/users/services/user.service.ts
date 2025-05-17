172eb1f9c8621f12c7d733cdcc28832fe74b7968
Antuanete01 <u202310358@upc.edu.pe>
2025-05-17 12:01:44 -0500

    Update user.service.ts

--- src/app/users/services/user.service.ts
+++ src/app/users/services/user.service.ts
@@ -1,53 +1,47 @@
 import { Injectable } from '@angular/core';
-import { environment } from '../../../environments/environment';
-import { BaseService } from '../../shared/services/base.service';
-import { User } from '../model/user.entity';
-import { BehaviorSubject, catchError, map, of, retry } from 'rxjs';
+import {environment} from '../../../environments/environment';
+import {BaseService} from '../../shared/services/base.service';
+import {User} from '../model/user.entity';
+import {BehaviorSubject, catchError, map, retry} from 'rxjs';
 
-const usersResourceEndpointPath = environment.usersEndpointPath || '';
+const usersResourceEndpointPath=environment.usersEndpointPath||'';
 
 @Injectable({
   providedIn: 'root'
 })
-export class UserService extends BaseService<User> {
+export class UserService extends BaseService<User>{
 
   private currentUserSubject = new BehaviorSubject<any>(this.getCurrentUser());
   public currentUser$ = this.currentUserSubject.asObservable();
 
   constructor() {
     super();
-    this.resourceEndpoint = usersResourceEndpointPath;
+    this.resourceEndpoint=usersResourceEndpointPath;
   }
 
-  override login(email: string, password: string) {
-    return this.http.get<any>(this.resourcePath(), this.httpOptions).pipe(
-      map(data => {
-        const usersArray = Array.isArray(data)
-          ? data
-          : data.users || [];
-
-        const user = usersArray.find((u: any) => u.email === email && u.password === password);
-
-        if (user) {
-          const parsedUser = new User(user);
-          localStorage.setItem('currentUser', JSON.stringify(parsedUser));
-          this.currentUserSubject.next(parsedUser);
-          return parsedUser;
+  login(email:string, password:string) {
+    const awr =${this.resourcePath()}?email=${email}&password=${password};
+    console.log('lolazo', awr);
+    return this.http.get<User[]>(${this.resourcePath()}?email=${email}&password=${password}, this.httpOptions).pipe(
+      retry(2), catchError(this.handleError), map(users => {
+        if (users.length) {
+          const user = users[0];
+          localStorage.setItem('currentUser', JSON.stringify(user));
+          this.currentUserSubject.next(user);
+          return user;
         }
-
         return null;
-      }),
-      catchError(this.handleError)
+      })
     );
   }
 
   logout() {
-    localStorage.removeItem('currentUser');
+    sessionStorage.removeItem('currentUser');
     this.currentUserSubject.next(null);
   }
 
   getCurrentUser() {
-    return JSON.parse(localStorage.getItem('currentUser') || 'null');
+    return JSON.parse(sessionStorage.getItem('currentUser') || 'null');
   }
 
   isLoggedIn(): boolean {
