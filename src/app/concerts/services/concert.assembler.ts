d9b0063f07d82c194e56986799c6c0490545270d
Antuanete01 <u202310358@upc.edu.pe>
2025-05-16 20:41:16 -0500

    Update concert.assembler.ts

--- src/app/concerts/services/concert.assembler.ts
+++ src/app/concerts/services/concert.assembler.ts
@@ -1,24 +1,24 @@
-import { ConcertResource, ConcertResponse } from './concert.response';
-import { Concert } from '../model/concert.entity';
+import {ConcertResource, ConcertResponse} from './concert.response';
+import {Concert} from '../model/concert.entity';
 
 export class ConcertAssembler {
 
-  static toEntityFromResource(resource: ConcertResource): Concert {
+  static toEntityFromResource(resource:ConcertResource):Concert {
     return {
       id: resource.id,
-      artistName: [resource.artist.name], // convertimos en array
-      genre: [resource.artist.genre],     // convertimos en array
+      artistName: resource.artist?.map(artist=>artist.name),
+      genre: resource.artist?.map(artist=>artist.genre),
       description: resource.description,
       image: resource.image,
       date: resource.date,
-      venueName: resource.venue?.name || '',
-      venueAddress: resource.venue?.address || '',
-      venueLocation: resource.venue?.location || { lat: 0, lng: 0 }
-    };
+      venueName: resource.venue?.name,
+      venueAddress: resource.venue?.address,
+      venueLocation: resource.venue.location
+    }
   }
 
-  static toEntitiesFromResponse(response: ConcertResponse): Concert[] {
-    return response.data.map(concert => this.toEntityFromResource(concert));
+  static toEntitiesFromResponse(response:ConcertResponse):Concert[] {
+    return response.data.map(concert=>this.toEntityFromResource(concert));
   }
 
 }
