52976df6f31185ba6fb4e444a19297e0de2399b5
Antuanete01 <u202310358@upc.edu.pe>
2025-05-16 20:38:03 -0500

    Update concert.assembler.ts

--- src/app/concerts/services/concert.assembler.ts
+++ src/app/concerts/services/concert.assembler.ts
@@ -1,24 +1,13 @@
-import {ConcertResource, ConcertResponse} from './concert.response';
-import {Concert} from '../model/concert.entity';
-
-export class ConcertAssembler {
-
-  static toEntityFromResource(resource:ConcertResource):Concert {
-    return {
-      id: resource.id,
-      artistName: resource.artist?.map(artist=>artist.name),
-      genre: resource.artist?.map(artist=>artist.genre),
-      description: resource.description,
-      image: resource.image,
-      date: resource.date,
-      venueName: resource.venue?.name,
-      venueAddress: resource.venue?.address,
-      venueLocation: resource.venue.location
-    }
-  }
-
-  static toEntitiesFromResponse(response:ConcertResponse):Concert[] {
-    return response.data.map(concert=>this.toEntityFromResource(concert));
-  }
-
+static toEntityFromResource(resource: ConcertResource): Concert {
+  return {
+    id: Number(resource.id),
+    artistName: [resource.artist?.name || ''],
+    genre: [resource.artist?.genre || ''],
+    description: resource.description,
+    image: resource.image,
+    date: resource.date,
+    venueName: resource.venue?.name,
+    venueAddress: resource.venue?.address,
+    venueLocation: resource.venue?.location
+  };
 }
