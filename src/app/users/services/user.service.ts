36b458d81dc37b52095552e1147638f51cf307cc
Antuanete01 <u202310358@upc.edu.pe>
2025-05-17 12:13:21 -0500

    Update user.service.ts

--- src/app/users/services/user.service.ts
+++ src/app/users/services/user.service.ts
@@ -1,45 +1,47 @@
 import { Injectable } from '@angular/core';
-import { BaseService } from '../../shared/services/base.service';
-import { User } from '../model/user.entity';
-import { BehaviorSubject, catchError, map, of, retry } from 'rxjs';
+import {environment} from '../../../environments/environment';
+import {BaseService} from '../../shared/services/base.service';
+import {User} from '../model/user.entity';
+import {BehaviorSubject, catchError, map, retry} from 'rxjs';
 
+const usersResourceEndpointPath=environment.usersEndpointPath||'';
 
 @Injectable({
   providedIn: 'root'
 })
-export class UserService extends BaseService<User> {
+export class UserService extends BaseService<User>{
 
-  private currentUserSubject = new BehaviorSubject<User | null>(this.getCurrentUser());
+  private currentUserSubject = new BehaviorSubject<any>(this.getCurrentUser());
   public currentUser$ = this.currentUserSubject.asObservable();
 
   constructor() {
     super();
-    this.resourceEndpoint = '/assets/data/db.json'; // Lee el archivo completo
+    this.resourceEndpoint=usersResourceEndpointPath;
   }
 
-  override login(email: string, password: string) {
-    return this.http.get<any>(this.resourcePath(), this.httpOptions).pipe(
-      map(data => {
-        const users = data.users || []; // 👈 accede a users del objeto
-        const user = users.find((u: User) => u.email === email && u.password === password);
-        if (user) {
+  login(email:string, password:string) {
+    const awr =${this.resourcePath()}?email=${email}&password=${password};
+    console.log('lolazo', awr);
+    return this.http.get<User[]>(${this.resourcePath()}?email=${email}&password=${password}, this.httpOptions).pipe(
+      retry(2), catchError(this.handleError), map(users => {
+        if (users.length) {
+          const user = users[0];
           localStorage.setItem('currentUser', JSON.stringify(user));
           this.currentUserSubject.next(user);
           return user;
         }
         return null;
-      }),
-      catchError(this.handleError)
+      })
     );
   }
 
   logout() {
-    localStorage.removeItem('currentUser');
+    sessionStorage.removeItem('currentUser');
     this.currentUserSubject.next(null);
   }
 
-  getCurrentUser(): User | null {
-    return JSON.parse(localStorage.getItem('currentUser') || 'null');
+  getCurrentUser() {
+    return JSON.parse(sessionStorage.getItem('currentUser') || 'null');
   }
 
   isLoggedIn(): boolean {
