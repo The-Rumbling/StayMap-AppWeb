906c710f7bb55e783523b55303676a49a5c4706b
Antuanete01 <u202310358@upc.edu.pe>
2025-05-16 20:28:58 -0500

    Update user.service.ts

--- src/app/users/services/user.service.ts
+++ src/app/users/services/user.service.ts
@@ -1,34 +1,34 @@
 import { Injectable } from '@angular/core';
-import {environment} from '../../../environments/environment';
-import {BaseService} from '../../shared/services/base.service';
-import {User} from '../model/user.entity';
-import {BehaviorSubject, catchError, map, retry} from 'rxjs';
+import { environment } from '../../../environments/environment';
+import { BaseService } from '../../shared/services/base.service';
+import { User } from '../model/user.entity';
+import { BehaviorSubject, catchError, map, retry } from 'rxjs';
 
-const usersResourceEndpointPath=environment.usersEndpointPath||'';
+const usersResourceEndpointPath = environment.usersEndpointPath || '';
 
 @Injectable({
   providedIn: 'root'
 })
-export class UserService extends BaseService<User>{
-
+export class UserService extends BaseService<User> {
   private currentUserSubject = new BehaviorSubject<any>(this.getCurrentUser());
   public currentUser$ = this.currentUserSubject.asObservable();
 
   constructor() {
     super();
-    this.resourceEndpoint=usersResourceEndpointPath;
+    this.resourceEndpoint = usersResourceEndpointPath;
   }
 
-  login(email:string, password:string) {
-    const awr =`${this.resourcePath()}?email=${email}&password=${password}`;
-    console.log('lolazo', awr);
-    return this.http.get<User[]>(`${this.resourcePath()}?email=${email}&password=${password}`, this.httpOptions).pipe(
-      retry(2), catchError(this.handleError), map(users => {
-        if (users.length) {
-          const user = users[0];
-          localStorage.setItem('currentUser', JSON.stringify(user));
-          this.currentUserSubject.next(user);
-          return user;
+  login(email: string, password: string) {
+    return this.http.get<any>(this.resourcePath(), this.httpOptions).pipe(
+      retry(2),
+      catchError(this.handleError),
+      map(response => {
+        const users: User[] = response.users || [];
+        const found = users.find(u => u.email === email && u.password === password);
+        if (found) {
+          localStorage.setItem('currentUser', JSON.stringify(found));
+          this.currentUserSubject.next(found);
+          return found;
         }
         return null;
       })
@@ -36,12 +36,12 @@ export class UserService extends BaseService<User>{
   }
 
   logout() {
-    sessionStorage.removeItem('currentUser');
+    localStorage.removeItem('currentUser');
     this.currentUserSubject.next(null);
   }
 
   getCurrentUser() {
-    return JSON.parse(sessionStorage.getItem('currentUser') || 'null');
+    return JSON.parse(localStorage.getItem('currentUser') || 'null');
   }
 
   isLoggedIn(): boolean {
