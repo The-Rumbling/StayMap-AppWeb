8d6d0a91fdf3a6559dc3957777a3ae682cad18c9
Antuanete01 <u202310358@upc.edu.pe>
2025-05-17 12:41:02 -0500

    Update app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,7 +3,7 @@ import {CommunityManagementComponent} from './community/pages/community-manageme
 import {ConcertMapComponent} from './concerts/pages/concert-map/concert-map.component';
 import {LoginComponent} from './public/pages/login/login.component';
 import {RegisterComponent} from './public/pages/register/register.component';
-import {ConcertManagementComponent} from './concerts/pages/concert-management/concert-management.component';
+import {ConcertManagementComponent} from './concerts/pages/concert-management/concert-view.component';
 
 export const routes: Routes = [
   { path: 'communities',  component: CommunityManagementComponent },
