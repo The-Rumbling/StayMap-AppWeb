9cbea2144b9ae09c116cbb513dc3176c32545b82
Antuanete01 <u202310358@upc.edu.pe>
2025-05-16 20:11:20 -0500

    Update community-management.component.ts

--- src/app/community/pages/community-management/community-management.component.ts
+++ src/app/community/pages/community-management/community-management.component.ts
@@ -1,28 +1,35 @@
-import {AfterViewInit, Component, inject, OnInit, ViewChild} from '@angular/core';
-
+import { AfterViewInit, Component, inject, OnInit, ViewChild } from '@angular/core';
 import {
-  MatCell, MatCellDef,
-  MatColumnDef,
-  MatHeaderCell,
-  MatHeaderCellDef, MatHeaderRow, MatHeaderRowDef, MatRow, MatRowDef,
-  MatTable,
+  MatPaginator
+} from '@angular/material/paginator';
+import {
+  MatSort
+} from '@angular/material/sort';
+import {
   MatTableDataSource
 } from '@angular/material/table';
-import {MatPaginator} from '@angular/material/paginator';
-import {MatSort, MatSortHeader} from '@angular/material/sort';
-import {NgClass, NgForOf, NgIf, NgStyle} from '@angular/common';
-import {MatIcon} from '@angular/material/icon';
-import {CommunityService} from '../../services/community.service';
-import {Community} from '../../model/community.entity';
+import {
+  NgForOf, NgIf, NgStyle
+} from '@angular/common';
+import {
+  CommunityService
+} from '../../services/community.service';
+import {
+  Community
+} from '../../model/community.entity';
 import {
   CommunityCreateAndEditComponent
 } from '../../components/community-create-and-edit/community-create-and-edit.component';
-import {MatButton} from '@angular/material/button';
-import {UserService} from '../../../users/services/user.service';
-import {User} from '../../../users/model/user.entity';
+import {
+  MatButton
+} from '@angular/material/button';
+import {
+  UserService
+} from '../../../users/services/user.service';
 
 @Component({
   selector: 'app-community-management',
+  standalone: true,
   imports: [
     NgForOf,
     NgStyle,
@@ -33,104 +40,99 @@ import {User} from '../../../users/model/user.entity';
   templateUrl: './community-management.component.html',
   styleUrl: './community-management.component.css'
 })
-export class CommunityManagementComponent implements OnInit, AfterViewInit{
-  isLoggedIn:boolean = false;
-  protected communityData!: Community;
-
+export class CommunityManagementComponent implements OnInit, AfterViewInit {
+  isLoggedIn: boolean = false;
+  protected communityData: Community = new Community({});
+  protected dataSource = new MatTableDataSource<Community>();
   protected columnsToDisplay: string[] = ['id', 'name', 'memberQuantity', 'actions'];
+  protected editMode = false;
+  protected formVisible = false;
 
-  protected editMode: boolean = false;
-  protected communityService: CommunityService = inject(CommunityService);
-  protected dataSource!: MatTableDataSource<any>;
-  @ViewChild(MatPaginator, {static: false})
-  protected paginator!: MatPaginator;
-  @ViewChild(MatSort)
-  protected sort!: MatSort;
+  protected communityService = inject(CommunityService);
+
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
 
   constructor(private userService: UserService) {
     this.userService.currentUser$.subscribe(user => {
-      this.isLoggedIn=!!user;
+      this.isLoggedIn = !!user;
     });
-    this.editMode = false;
-    this.communityData = new Community({});
-    this.dataSource = new MatTableDataSource();
-    console.log(this.communityData);
-  }
-
-  ngAfterViewInit() {
-    this.dataSource.paginator = this.paginator;
-    this.dataSource.sort = this.sort;
   }
 
   ngOnInit(): void {
     this.getAllCommunities();
   }
-  private getAllCommunities() {
-    this.communityService.getAll().subscribe((response: Array<Community>) => {
-      console.log(' Comunidades cargadas desde el JSON:', response);
-      this.dataSource.data = response;
+
+  ngAfterViewInit(): void {
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
+  }
+
+  private getAllCommunities(): void {
+    this.communityService.getAll().subscribe({
+      next: (response: Community[]) => {
+        console.log('✅ Comunidades cargadas:', response);
+        this.dataSource.data = response;
+      },
+      error: error => {
+        console.error('❌ Error cargando comunidades:', error);
+      }
     });
   }
-  private createCommunity() {
-    if (!this.communityData.id) {
-      this.communityData.id = Date.now();
+
+  private createCommunity(): void {
+    this.communityData.id = Date.now(); // Crear ID si no tiene
+    this.dataSource.data.push(this.communityData);
+    this.dataSource.data = [...this.dataSource.data];
+  }
+
+  private updateCommunity(): void {
+    const index = this.dataSource.data.findIndex(c => c.id === this.communityData.id);
+    if (index !== -1) {
+      this.dataSource.data[index] = this.communityData;
+      this.dataSource.data = [...this.dataSource.data];
     }
-
-    this.communityService.create(this.communityData).subscribe(() => {
-      this.getAllCommunities();
-    });
   }
 
-  private updateCommunity() {
-    let communityToUpdate = this.communityData;
-    this.communityService.update(communityToUpdate.id, communityToUpdate).subscribe((response: Community) => {
-      let index = this.dataSource.data.findIndex((community: Community) => community.id === response.id);
-      this.dataSource.data[index] = response;
-      this.dataSource.data = this.dataSource.data;
-    });
+  private deleteCommunity(id: number): void {
+    this.dataSource.data = this.dataSource.data.filter(c => c.id !== id);
   }
 
-
-  private deleteCommunity(id: number) {
-    this.communityService.delete(id).subscribe(() => {
-      this.dataSource.data = this.dataSource.data.filter((community: Community) => community.id !== id);
-    });
-  }
-
-  protected onEditItem(item: Community) {
+  protected onEditItem(item: Community): void {
     this.editMode = true;
-    this.communityData = item;
+    this.communityData = { ...item }; // Para evitar editar en tiempo real
+    this.formVisible = true;
   }
 
-  protected onDeleteItem(item: Community) {
+  protected onDeleteItem(item: Community): void {
     this.deleteCommunity(item.id);
   }
 
-  protected onCancelRequested() {
+  protected onCancelRequested(): void {
     this.resetEditState();
     this.formVisible = false;
   }
 
-  private resetEditState() {
+  private resetEditState(): void {
     this.communityData = new Community({});
     this.editMode = false;
   }
 
-  protected onCommunityAddRequested(item: Community) {
+  protected onCommunityAddRequested(item: Community): void {
     this.communityData = item;
     this.createCommunity();
     this.resetEditState();
-  }
-
-  protected onCommunityUpdateRequested(item: Community) {
-    this.communityData = item;
-    this.updateCommunity();
     this.formVisible = false;
   }
 
-  protected formVisible = false;
+  protected onCommunityUpdateRequested(item: Community): void {
+    this.communityData = item;
+    this.updateCommunity();
+    this.resetEditState();
+    this.formVisible = false;
+  }
 
-  protected onAddCommunityClicked() {
+  protected onAddCommunityClicked(): void {
     this.resetEditState();
     this.formVisible = true;
   }
