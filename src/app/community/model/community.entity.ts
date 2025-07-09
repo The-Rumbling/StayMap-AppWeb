8e90b6886638e14e9d0edd574119c1610b47b435
Rei <zrena2005@gmail.com>
2025-07-09 17:57:55 -0500

    Update community.entity.ts

--- src/app/community/model/community.entity.ts
+++ src/app/community/model/community.entity.ts
@@ -3,7 +3,7 @@ export class Community {
   name:string;
   memberQuantity:string;
   image: string;
-  
+   
   constructor(community:{id?: number, name?: string, memberQuantity?:string, image?: string}) {
     this.id = community.id || 0;
     this.name = community.name|| '';
